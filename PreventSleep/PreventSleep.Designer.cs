456415c40a82e388d7d55774a78024578ce9f7fd
yoggy <yoggy0@gmail.com>
2016-09-29 17:19:30 +0900

    initial commit

---
+++ PreventSleep/PreventSleep.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace PreventSleep
+{
+    partial class PreventSleep
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PreventSleep));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(129, 34);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(128, 30);
+            this.quitToolStripMenuItem.Text = "quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 196);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+    }
+}
+
---
+++ PreventSleep/PreventSleep.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace PreventSleep
+{
+    public partial class PreventSleep : Form
+    {
+        [FlagsAttribute]
+        public enum EXECUTION_STATE : uint
+        {
+            NULL = 0,
+            ES_SYSTEM_REQUIRED = 0x00000001,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_CONTINUOUS = 0x80000000,
+        }
+
+        [DllImport("kernel32.dll")]
+        extern static EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+        public PreventSleep()
+        {
+            InitializeComponent();
+
+            var timer = new Timer();
+            timer.Tick += new EventHandler(this.OnTimer);
+            timer.Interval = 10 * 1000;
+            timer.Start();
+
+            notifyIcon1.Text = "PreventSleep";
+
+            // prevent system sleep
+            SetThreadExecutionState(EXECUTION_STATE.ES_SYSTEM_REQUIRED | EXECUTION_STATE.ES_CONTINUOUS);
+        }
+
+        private void OnTimer(object sender, EventArgs e)
+        {
+            // prevent display from turning off
+            SetThreadExecutionState(EXECUTION_STATE.ES_DISPLAY_REQUIRED);
+        }
+
+        //
+        // see also... http://dobon.net/vb/dotnet/form/hideformwithtrayicon.html
+        //
+        protected override CreateParams CreateParams
+        {
+            [System.Security.Permissions.SecurityPermission(
+                System.Security.Permissions.SecurityAction.LinkDemand,
+                Flags = System.Security.Permissions.SecurityPermissionFlag.UnmanagedCode)]
+            get
+            {
+                const int WS_EX_TOOLWINDOW = 0x80;
+                const long WS_POPUP = 0x80000000L;
+                const int WS_VISIBLE = 0x10000000;
+                const int WS_SYSMENU = 0x80000;
+                const int WS_MAXIMIZEBOX = 0x10000;
+
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle = WS_EX_TOOLWINDOW;
+                cp.Style = unchecked((int)WS_POPUP) |
+                    WS_VISIBLE | WS_SYSMENU | WS_MAXIMIZEBOX;
+                cp.Width = 0;
+                cp.Height = 0;
+
+                return cp;
+            }
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+
+            // clear ES_SYSTEM_REQUIRED
+            SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+
+            Application.Exit();
+        }
+    }
+}
